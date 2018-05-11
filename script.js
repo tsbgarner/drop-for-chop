635436e24fb32422eddd9051dd966bd97ed16c1e
Brett Garner <bgarner@thinkspatial.com.au>
2018-05-11 15:53:54 +1000

    Added a secondary container.

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ $(function() {
     var value = $(this).val();
 
     // Hides all content views on the right.
-    $('.item-content').hide();
+    $('.container1 .item-content').hide();
+
+    // Shows the content view matching the id of the selected item in the dropdown.
+    $('#content-item' + value).show();
+  });
+
+  $('#select2').change(function() {
+    // Gets the value of selected item in the dropdown.
+    var value = $(this).val();
+
+    // Hides all content views on the right.
+    $('.container2 .item-content').hide();
 
     // Shows the content view matching the id of the selected item in the dropdown.
     $('#content-item' + value).show();
